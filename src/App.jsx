2f24a6d43bd589abf99aaad5032f8b13dd1e0b47
Rahul Gurjar <rahulgurjar@Rahuls-MacBook-Air.local>
2025-03-23 15:43:43 +0530

    Resolve build warnings and confirm successful local build

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,11 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Component Imports
-import LandingPage from "../components/LandingPage";
-import Footer from "../components/Footer";
-import CryptoGraphs from "../components/CryptoGraphs";
+import Homebutton from "../components/Home";
+import AboutUs from "../components/AboutUs"; // Fixed capitalization
 import TeamPage from "../components/TeamPage";
 import Events from "../components/Events";
-import Homebutton from "../components/Home";
-import AboutUs from "../components/Aboutus";
-import ContactUs from "../components/Contactus";
+import ContactUs from "../components/ContactUs";
 
 // Icon Imports
 import { Home } from "lucide-react";
@@ -19,25 +16,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Root Route */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Homebutton />
-            </>
-          }
-        />
-        
-        {/* Main Navigation Routes */}
+        <Route path="/" element={<Homebutton />} />
         <Route path="/home" element={<Homebutton />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/team" element={<TeamPage />} />
         <Route path="/events" element={<Events />} />
-        
-        {/* Contact Routes */}
         <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/contact" element={<ContactUs />} /> {/* Alias for contact-us */}
+        <Route path="/contact" element={<ContactUs />} />
       </Routes>
     </Router>
   );
