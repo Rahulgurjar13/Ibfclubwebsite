7272b7657557ce9eaf2a62a084b177856684c914
Rahul Gurjar <rahulgurjar@Rahuls-MacBook-Air.local>
2025-03-23 15:37:46 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,43 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Component Imports
 import LandingPage from "../components/LandingPage";
 import Footer from "../components/Footer";
 import CryptoGraphs from "../components/CryptoGraphs";
 import TeamPage from "../components/TeamPage";
 import Events from "../components/Events";
-import { Home } from "lucide-react";
 import Homebutton from "../components/Home";
 import AboutUs from "../components/Aboutus";
 import ContactUs from "../components/Contactus";
 
+// Icon Imports
+import { Home } from "lucide-react";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Define routes for each page */}
-        <Route 
-          path="/" 
+        {/* Root Route */}
+        <Route
+          path="/"
           element={
             <>
-            <Homebutton></Homebutton>
+              <Homebutton />
             </>
-          } 
+          }
         />
-        {/* Add additional routes as needed */}
-        contact-us
+        
+        {/* Main Navigation Routes */}
         <Route path="/home" element={<Homebutton />} />
-        <Route path="/team" element={<TeamPage />} />
-        <Route path="/Events" element={<Events />} />
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/team" element={<TeamPage />} />
+        <Route path="/events" element={<Events />} />
+        
+        {/* Contact Routes */}
         <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/contact" element={<ContactUs />} />
-        
-        
+        <Route path="/contact" element={<ContactUs />} /> {/* Alias for contact-us */}
       </Routes>
     </Router>
   );
